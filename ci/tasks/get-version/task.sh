a1b851a75aa9f69e501180747fa4d7171624a4c9
Mark Alston <malston@pivotal.io>
2017-08-17 10:57:28 -0600

    Use project version instead of generated version.
    The reason we don't remove the generated version task is because
    the build-and-upload task depends on a version resource as one
    of its inputs. So instead we overwrite the generated version in
    the local get-version task. We could write the version out to a
    different location so it could be reused across tasks and you
    could pick either output as input into your task that requires
    a version. More to come on that soon...

---
+++ ci/tasks/get-version/task.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -o errexit
+
+CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+export ROOT_FOLDER=$( pwd )
+export REPO_RESOURCE=repo
+export TOOLS_RESOURCE=tools
+export VERSION_RESOURCE=version
+export FUNCTIONS_FOLDER=${ROOT_FOLDER}/${TOOLS_RESOURCE}/functions
+
+echo "Root folder is [${ROOT_FOLDER}]"
+echo "Repo resource folder is [${REPO_RESOURCE}]"
+echo "Tools resource folder is [${TOOLS_RESOURCE}]"
+echo "Version resource folder is [${VERSION_RESOURCE}]"
+echo "Functions folder is [${FUNCTIONS_FOLDER}]"
+
+PIPELINE_VERSION=$( cat ${ROOT_FOLDER}/${VERSION_RESOURCE}/version )
+echo "Generated pipeline version is [${PIPELINE_VERSION}]"
+
+[[ -f "${FUNCTIONS_FOLDER}/projectType/pipeline-jvm.sh" ]] && source "${FUNCTIONS_FOLDER}/projectType/pipeline-jvm.sh" || \
+    echo "No ${FUNCTIONS_FOLDER}/projectType/pipeline-jvm.sh found"
+
+cd ${ROOT_FOLDER}/${REPO_RESOURCE}
+
+PROJECT_VERSION=$( retrieveVersion )
+echo "Project version is [${PROJECT_VERSION}]"
+
+echo "${PROJECT_VERSION}" > ${ROOT_FOLDER}/${VERSION_RESOURCE}/version
