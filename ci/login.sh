bb9e3cc814c931c6906652834a6620ff58365feb
Mark Alston <malston@pivotal.io>
2017-08-24 13:02:53 -0600

    Use whats_my_ip.sh in login.sh

--- ci/login.sh
+++ ci/login.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 TARGET=${1:-docker}
-ROOT_ADDRESS=localhost
-CONCOURSE_URL=${2:-"http://${ROOT_ADDRESS}:8080"}
-CONCOURSE_USER=${3:-concourse}
-CONCOURSE_PASSWORD=${4:-changeme}
-TEAM=${5:-main}
+ROOT_ADDRESS=${2:-`./whats_my_ip.sh`}
+CONCOURSE_URL=${3:-"http://${ROOT_ADDRESS}:8080"}
+CONCOURSE_USER=${4:-concourse}
+CONCOURSE_PASSWORD=${5:-changeme}
+TEAM=${6:-main}
 
 fly -t "${TARGET}" login -c "${CONCOURSE_URL}" -u "${CONCOURSE_USER}" -p "${CONCOURSE_PASSWORD}" -n "${TEAM}"
