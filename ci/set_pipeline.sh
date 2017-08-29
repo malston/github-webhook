d6326fabc1a7b5ff953a45117f83d60619109399
Mark Alston <malston@pivotal.io>
2017-08-29 10:46:32 -0600

    Use tasks from pcf-apptx-pipelines

--- ci/set_pipeline.sh
+++ ci/set_pipeline.sh
@@ -6,4 +6,4 @@ PIPELINE_NAME=${1:-github-webhook}
 ALIAS=${2:-docker}
 CREDENTIALS=${3:-credentials.yml}
 
-echo y | fly -t "${ALIAS}" sp -p "${PIPELINE_NAME}" -c "${__DIR}/pipeline.yml" -l "${__DIR}/${CREDENTIALS}"
+fly -t "${ALIAS}" sp -p "${PIPELINE_NAME}" -c "${__DIR}/pipeline.yml" -l "${__DIR}/${CREDENTIALS}" -n
